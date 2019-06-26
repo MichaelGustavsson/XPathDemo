4db5d0130b6fc95e7d4cda604581d514c55cf7bb
Michael Gustafsson <michael.gustavsson@skaftoskorpan.se>
2019-06-26 07:45:07 +0200

    Initial Commit

---
+++ XPath/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.XPath;
+
+namespace XPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XPathNavigator nav;
+            XPathDocument docNav;
+            XPathNodeIterator NodeIter;
+            String query;
+
+            // Load the XML.
+            docNav = new XPathDocument(@"C:\Dev\Repos\XPathDemo\Flights.xml");
+
+            // Create a navigator to query with XPath.
+            nav = docNav.CreateNavigator();
+
+            query = "/Flights/Flight/Origin[text()='LHR']";
+            NodeIter = nav.Select(query);
+
+            //Iterate through the results showing the element value.
+            while (NodeIter.MoveNext())
+            {
+                Console.WriteLine(NodeIter.Current.OuterXml);
+                // Console.WriteLine("Airline: {0}", NodeIter.Current.Value);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
